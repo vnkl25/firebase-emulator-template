6c14f26184f70a209abaf8b16a04364cce477b47
vnkl25 <vykhanhle9971@gmail.com>
2026-02-18 02:04:06 -0600

    Initial commit

---
+++ public/firebase-config.js
@@ -0,0 +1,36 @@
+// Import the Firebase SDKs 
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.4.0/firebase-app.js";
+import { getAuth, GoogleAuthProvider, signInWithPopup, signOut, onAuthStateChanged, connectAuthEmulator } from "https://www.gstatic.com/firebasejs/12.4.0/firebase-auth.js";
+import { getFirestore, doc, setDoc, collection, getDocs, connectFirestoreEmulator } from "https://www.gstatic.com/firebasejs/12.4.0/firebase-firestore.js";
+import { getStorage, ref, uploadBytes, getDownloadURL, listAll, connectStorageEmulator } from "https://www.gstatic.com/firebasejs/12.4.0/firebase-storage.js";
+
+
+// Your Firebase configuration
+const firebaseConfig = {
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+const storage = getStorage(app);
+
+// Only connect to emulators when running locally
+if (location.hostname === "localhost") {
+  connectAuthEmulator(auth, "http://localhost:9099");
+  connectFirestoreEmulator(db, "localhost", 8081);
+  connectStorageEmulator(storage, "localhost", 9199);
+
+}
+
+// Export so other scripts can use it
+export { auth, GoogleAuthProvider, signInWithPopup, signOut, onAuthStateChanged };
+export { db, doc, setDoc, collection, getDocs };
+export { storage, ref, uploadBytes, getDownloadURL, listAll } 
+
